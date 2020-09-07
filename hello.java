8141d0fdec10bfbdb334dfeb42dc0e7255a7d5e5
Peter Liu <peter.liu@ericsson.com>
2020-09-07 15:15:15 +0800

    modify happy in xvpb

--- hello.java
+++ hello.java
@@ -3,6 +3,7 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("happpy new year");
 		System.out.println("happpy my self");
+		System.out.println("happpy in xvpb");
 	}
 
 }
